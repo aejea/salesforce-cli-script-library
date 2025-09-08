4b81e349427f0374159e5df2b8ee173f0ac42f34
Jeff Arwady <Jeff.Arwady@american-equity.com>
2025-09-08 17:54:52 -0400

    Updated retrieve xml script

--- retrieve-manifest.bat
+++ retrieve-manifest.bat
@@ -10,25 +10,20 @@ cd C:\Users\%USERNAME%\bin\salesforce-devops-center
 :: ==================
 
 set /p ORG_ALIAS=Org alias:
-set /p TYPE=Is this a bugfix, story, or task? (example: story)
+set /p TYPE=Is this a bugfix, story, or task? (example: story):
 set /p MANIFEST=Manifest name (example: se-1234):
 
 :: =========================
 :: = Retrieve the manifest =
 :: =========================
 
-git checkout -b feature/%TYPE%/%MANIFEST%
-IF %ERRORLEVEL% NEQ 0 (
-    echo Branch exists. Checking it out...
-    git checkout feature/%TYPE%/%MANIFEST%
-    IF %ERRORLEVEL% NEQ 0 (
-        echo Failed to checkout existing branch.
-        pause
-        goto :end
-    )
-)
-pause
+git checkout dev
 cls
+git checkout -b feature/%TYPE%/%MANIFEST%
+cls
+git checkout feature/%TYPE%/%MANIFEST%
+cls
+Retrieving from %ORG_ALIAS%...
 call sf project retrieve start --ignore-conflicts --target-org %ORG_ALIAS% --manifest manifest\%MANIFEST%.xml
 pause
 
