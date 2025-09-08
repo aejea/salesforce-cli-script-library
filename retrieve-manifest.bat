b0161d8cb130e93fe2b7e2afef9630ea35a12dc6
Jeff Arwady <Jeff.Arwady@american-equity.com>
2025-09-08 14:03:04 -0400

    Updated Release-Manifest Scriptr

--- retrieve-manifest.bat
+++ retrieve-manifest.bat
@@ -9,13 +9,19 @@ cd ..\salesforce-devops-center
 :: = Get User Input =
 :: ==================
 
-set /p ORG_ALIAS=Org alias: 
+set /p ORG_ALIAS=Org alias:
+set /p TYPE=Is this a bugfix, story, or task? (example: story)
 set /p MANIFEST=Manifest name (example: se-1234):
 
 :: =========================
 :: = Retrieve the manifest =
 :: =========================
 
+git checkout -b feature/%TYPE%/%MANIFEST%
+if ERRORLEVEL 1 (
+    git checkout feature/%TYPE%/%MANIFEST%
+)
+pause
+cls
 call sf project retrieve start --ignore-conflicts --target-org %ORG_ALIAS% --manifest manifest\%MANIFEST%.xml
-
 pause
